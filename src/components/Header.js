a6cc7f065c99d36fa89f4caaa58578cefd9ef4b3
Abhishek Samariya <asamariya@gmail.com>
2019-11-11 14:48:57 +1100

    Updated header to include studyLevel and degreeCode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from 'react';
 
 export const Header = ({url}) => {
 	const [degreeTitle, setDegreeTitle] = useState('');
-	const fetchTitle = async () => {
+	const [studyLevel, setStudyLevel] = useState('');
+	const [degreeCode, setDegreeCode] = useState('');
+
+	const capitalizeFirstLetter = str => {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	};
+
+	const fetchDegree = async () => {
 		try {
 			let response = await fetch(url);
 			let data = await response.json();
 			setDegreeTitle(data.title);
+			setStudyLevel(data.studyLevel);
+			setDegreeCode(data.code);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
 	useEffect(() => {
-		fetchTitle();
+		fetchDegree();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
 		<div className="header">
+			<p>
+				{capitalizeFirstLetter(studyLevel)} | {degreeCode}
+			</p>
 			<h1>{degreeTitle}</h1>
 		</div>
 	);
