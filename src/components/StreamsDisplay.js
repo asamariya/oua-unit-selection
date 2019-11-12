ca7a7c80891f5401576c3e330f4426b8a095cee9
Abhishek Samariya <asamariya@gmail.com>
2019-11-12 20:58:56 +1100

    Mandatory PreReq
    - Added mandatory prereq unit codes to each subject display
    - Added styles to add padding and border to streams display

--- src/components/StreamsDisplay.js
+++ src/components/StreamsDisplay.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import Subject from './Subject';
+import React from "react";
+import Subject from "./Subject";
 
-const StreamsDisplay = ({title, opSubjects, reqSubjects}) => {
-	return (
-		<div>
-			<h4>{title}</h4>
-			{opSubjects.length > 0 && (
-				<div className="opSubjects">
-					<h5>Optional Subjects:</h5>
-					{opSubjects.map((subject, index) => (
-						<Subject subject={subject} key={index} />
-					))}
-				</div>
-			)}
-			<div className="reqSubjects">
-				<h5>Required Subjects:</h5>
-				{reqSubjects.map((subject, index) => (
-					<Subject subject={subject} key={index} />
-				))}
-			</div>
-		</div>
-	);
+const StreamsDisplay = ({ title, opSubjects, reqSubjects }) => {
+  return (
+    <div className="streams">
+      <div className="stream-title py-2">
+        <h4>{title}</h4>
+      </div>
+      {opSubjects.length > 0 && (
+        <div className="opSubjects">
+          <h5>Optional Subjects:</h5>
+          {opSubjects.map((subject, index) => (
+            <Subject subject={subject} key={index} />
+          ))}
+        </div>
+      )}
+      <div className="reqSubjects">
+        <h5>Required Subjects:</h5>
+        {reqSubjects.map((subject, index) => (
+          <Subject subject={subject} key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default StreamsDisplay;
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,27 +1,39 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-const Subject = ({subject}) => {
-	const subjectUrl = `https://catalog.prd1.env.opendev.edu.au/v3/subjects/${subject.unitCode}`;
+const Subject = ({ subject }) => {
+  const subjectUrl = `https://catalog.prd1.env.opendev.edu.au/v3/subjects/${subject.unitCode}`;
 
-	const [subjectInfo, setSubjectInfo] = useState({});
-	const fetchSubject = async () => {
-		let response = await fetch(subjectUrl);
-		let data = await response.json();
-		setSubjectInfo(data);
-	};
+  const [subjectInfo, setSubjectInfo] = useState({});
+  const fetchSubject = async () => {
+    let response = await fetch(subjectUrl);
+    let data = await response.json();
+    setSubjectInfo(data);
+  };
 
-	useEffect(() => {
-		fetchSubject();
-	}, []);
+  useEffect(() => {
+    fetchSubject();
+  }, []);
 
-	return (
-		<div>
-			<p>
-				<small>{subjectInfo.subjectCode}</small> | <strong>{subjectInfo.title}</strong>
-			</p>
-		</div>
-	);
+  const toSentence = arr => {
+    return arr.slice(0, arr.length + 1).join(", ");
+  };
+
+  return (
+    <div className="subject">
+      <p>
+        <small>{subjectInfo.subjectCode}</small> |{" "}
+        <strong>{subjectInfo.title}</strong>
+      </p>
+
+      {subjectInfo.eligibility && subjectInfo.eligibility.mandatory.length > 0 && (
+        <p>
+          Mandatory PreReq: <br />{" "}
+          {toSentence(subjectInfo.eligibility.mandatory)}
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default Subject;
